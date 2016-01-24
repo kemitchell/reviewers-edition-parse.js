5d9305f21abb5a8da2713aa85d3bf6b122be3e57
Kyle Mitchell <kyle@kemitchell.com>
2016-01-23 21:46:04 -0800

    Remove extra variable declaration

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function semanticEditionParse(argument) {
   var parsed = regularExpression.exec(argument)
   var returned = { }
   components.forEach(function(component, index) {
-    var group = ( index + 1 )
-    var value = parsed[group]
+    var value = parsed[( index + 1 )]
     if (value !== undefined) {
       returned[component] = parseInt(value) } })
   return returned }
